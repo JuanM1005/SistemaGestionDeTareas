1bc0d8fcc515fca0366c2d885d0d3d5cb5125596
JuanM1005 <juan.aguirre5548@alumnos.udg.mx>
2023-11-25 20:12:17 -0600

    Se agrego el Proyecto

---
+++ main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include "FUNCIONES.H"
+
+/*Aguirre Mares Juan Antonio -
+    Programación Estructurada*/
+
+int main() {
+    struct tarea Tareas[MAX_TAREAS];
+    int numTareas = 0, opc;
+
+    do {
+        system("cls");
+        menu_principal();
+        scanf("%d", &opc);
+
+        switch (opc) {
+            case 1:
+                system("cls");
+                agregarTareas(Tareas, &numTareas);
+                break;
+            case 2:
+                system("cls");
+                listarTareas(Tareas, numTareas);
+                system("pause");
+                break;
+            case 3:
+                system("cls");
+                TareasPendientes(Tareas, numTareas);
+                system("pause");
+                break;
+            case 4:
+                system("cls");
+                tareaCompletada(Tareas, numTareas);
+                system("pause");
+                break;
+            case 5:
+                system("cls");
+                buscarTareaPorTitulo(Tareas, numTareas, 0);
+                system("pause");
+                break;
+            case 6:
+                system("cls");
+                imprime_salida();
+                break;
+            default:
+                printf("Error opci%cn incorrecta. Int%cntelo de nuevo.\n", 162, 130);
+        }
+
+    } while (opc != 6);
+
+    return 0;
+}
